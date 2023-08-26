6a274c2753e27d1a4075d21a50ec1413cd81cf5c
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-08-26 20:15:27 -0300

    fix: prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { PrismaService } from './prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
