1de863c165263c77622c4039c69e4a21d7fa756c
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-08-28 16:34:47 -0300

    integration tests of route POST /media and GET /health

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { PrismaModule } from '../src/prisma/prisma.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, PrismaModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = await moduleFixture.resolve(PrismaService);
+    await prisma.media.deleteMany();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/health (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect("I'm okay!");
   });
+
+  it('/media (POST) - valid body', async () => {
+    await request(app.getHttpServer())
+      .post('/medias')
+      .send({
+        title: "Instagram",
+        username: "myusername",
+      })
+      .expect(HttpStatus.CREATED);
+  });
+  
+  it('/media (POST) - invalid body', async () => {
+    await request(app.getHttpServer())
+      .post('/medias')
+      .send({
+        username: "myusername",
+      })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+  
+  it('/media (POST) - title and username already exists', async () => {
+    await request(app.getHttpServer())
+      .post('/medias')
+      .send({
+        title: "Instagram",
+        username: "myusername",
+      })
+      .expect(HttpStatus.CREATED);
+      
+      await request(app.getHttpServer())
+      .post('/medias')
+      .send({
+        title: "Instagram",
+        username: "myusername",
+      })
+      .expect(HttpStatus.CONFLICT);
+  });
+
 });
